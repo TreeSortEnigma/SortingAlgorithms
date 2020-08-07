396fc7623ae0deff83cbaf512b73b57ec314f959
abigailswinson <68247624+abigailswinson@users.noreply.github.com>
2020-08-07 00:32:18 -0700

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,21 +1,55 @@
-class bubbleSort {
-  public static int[] bubbleSort(int[] inputArr) {
-    //references:
-    //https://en.wikipedia.org/wiki/Bubble_sort
-    //zyBooks chapter 17
+public class bubbleSort extends Sortable {
 
+  long delay;
+
+  public bubbleSort(int delay) {
+    this.delay = delay;
+  }
+
+  @Override //this is the entry point. SortManager is passed in to allow access to the bars being redrawn.
+  public void beginSort(int[] array, SortManager sortManager) {
+      sort(array, sortManager);
+  }
+
+  void sort(int[] inputArr, SortManager sortManager) {
     int i, j, tempInt= 0;
     int length = inputArr.length;
+    for( i = 0; i < length-1; i++) //loops over the indices of the array
+    {
+      for( j = 0; j < length-1-i; j++) //loops over the sub-indices
+      {
 
-    for( i = 0; i < length-1; i++) {
-      for( j = 0; j < length-1-i; j++) {
         if(inputArr[j] > inputArr[j+1]) {
+
+          sortManager.highlightedElements()[j]= "red";
+          sortManager.getUIPanel().repaint();
+
+
           tempInt = inputArr[j+1];
           inputArr[j+1] = inputArr[j];
           inputArr[j] = tempInt;
+
+
+          sortManager.swapIndices( j, j+1, inputArr, sortManager );
+
+          try {
+            Thread.sleep(sortManager.getDelay());
+          } catch (InterruptedException e) {
+            System.out.println(e);
+          }
+
         }
+
+
+      sortManager.highlightedElements()[j] ="";
+
+      sortManager.getUIPanel().repaint();
+
+
       }
     }
-    return inputArr;
+
   }
+
+
 }
