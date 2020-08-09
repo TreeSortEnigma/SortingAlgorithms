cccc4f1cd63f6c86f17eda0862a76156e8c277ba
hedy-wang <48496010+hedy-wang@users.noreply.github.com>
2020-08-08 21:19:05 -0700

    added header

--- src/main/java/edu/sdccd/cisc191/groupg/sortingalgorithms/algorithms/Sortable.java
+++ src/main/java/edu/sdccd/cisc191/groupg/sortingalgorithms/algorithms/Sortable.java
@@ -1,3 +1,10 @@
+// File:         Sortable.java
+// Created:      07/16/2020
+// Last Changed: 08/08/2020
+// Author:       
+// 
+//Description:    This file contains the necessary code to complete a Sortable Algorithm.
+
 package edu.sdccd.cisc191.groupg.sortingalgorithms.algorithms;
 
 import edu.sdccd.cisc191.groupg.sortingalgorithms.SortManager;
