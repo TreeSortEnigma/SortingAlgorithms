24509f9cd08d374a683eac9511d6d07f3113a0bd
hedy-wang <48496010+hedy-wang@users.noreply.github.com>
2020-08-08 21:17:08 -0700

    Update SelectionSort.java

--- src/main/java/edu/sdccd/cisc191/groupg/sortingalgorithms/algorithms/SelectionSort.java
+++ src/main/java/edu/sdccd/cisc191/groupg/sortingalgorithms/algorithms/SelectionSort.java
@@ -1,3 +1,10 @@
+// File:         SelectionSort.java
+// Created:      07/16/2020
+// Last Changed: 08/08/2020
+// Author:       
+// 
+//Description:    This file contains the necessary code to complete a SelectionSort Algorithm.
+
 package edu.sdccd.cisc191.groupg.sortingalgorithms.algorithms;
 
 import edu.sdccd.cisc191.groupg.sortingalgorithms.SortManager;
