d2aae50b59b3575ad562ca0df45337039811ab92
hedy-wang <48496010+hedy-wang@users.noreply.github.com>
2020-08-08 21:07:39 -0700

    Update QuickSort.java

--- src/main/java/edu/sdccd/cisc191/groupg/sortingalgorithms/algorithms/QuickSort.java
+++ src/main/java/edu/sdccd/cisc191/groupg/sortingalgorithms/algorithms/QuickSort.java
@@ -1,3 +1,10 @@
+// File:         QuickSort.java
+// Created:      07/16/2020
+// Last Changed: 08/08/2020
+// Author:       
+// 
+//Description:    This file contains the necessary code to complete a QuickSort Algorithm.
+
 package edu.sdccd.cisc191.groupg.sortingalgorithms.algorithms;
 
 import edu.sdccd.cisc191.groupg.sortingalgorithms.SortManager;
