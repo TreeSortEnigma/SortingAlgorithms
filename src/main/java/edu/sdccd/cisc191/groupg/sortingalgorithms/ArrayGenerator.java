fa8f603cbf9217d1fadc579b032e9156357688a2
abigailswinson <68247624+abigailswinson@users.noreply.github.com>
2020-08-08 21:46:44 -0700

    Added header

--- src/main/java/edu/sdccd/cisc191/groupg/sortingalgorithms/ArrayGenerator.java
+++ src/main/java/edu/sdccd/cisc191/groupg/sortingalgorithms/ArrayGenerator.java
@@ -1,3 +1,10 @@
+// File:         ArrayGenerator.java
+// Created:      
+// Last Changed: 08/08/2020
+// Author:       Hailun Xu
+// 
+//Description:    This file contains the necessary code to generate a random Array when the user hits “generate".
+
 package edu.sdccd.cisc191.groupg.sortingalgorithms;
 
 import java.util.Random;
